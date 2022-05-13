fe1ee90c7ba5a92ae7aafc7121c17829cf5ce6ef
matehus-henrique <mtheusriki@gmail.com>
2022-05-13 14:34:53 -0300

    rest-cards

---
+++ src/main/java/com/keyworks/DesafioKeyParaDesenvolvedorApplication.java
@@ -0,0 +1,13 @@
+package com.keyworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioKeyParaDesenvolvedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioKeyParaDesenvolvedorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/keyworks/resources/CardsResouces.java
@@ -0,0 +1,17 @@
+package com.keyworks.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cards")
+public class CardsResouces {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String listar() {
+		return "Rest está funcionando";
+	}
+	
+	
+}
